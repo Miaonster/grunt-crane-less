9adfccf57d415615c06ca43bdbe9bcf661d5195f
Witcher42 <dy.lengleng@gmail.com>
2014-01-03 21:05:30 +0800

    Update to 0.1.4, add image version

--- builder/less.js
+++ builder/less.js
@@ -1,25 +1,25 @@
-var path = require('path');
-var less = require('less');
+var path    = require('path');
+var URL     = require('url');
+var less    = require('less');
+var rework  = require('rework');
 var promise = require('../util/promise');
+
+var HTTP_FILE_RE = /^(https?:\/\/.*?\/)/;
 
 var Parser = less.Parser;
 
 module.exports = function (grunt) {
-    var minify   = grunt.config('compress'),
-        rootpath = grunt.config('rootpath'),
-        dest     = grunt.config('dest'),
-        src      = grunt.config('src'),
-        tmp      = grunt.config('tmp') || 'tmp';
-
+    var minify   = grunt.config('compress');
+    var rootpath = grunt.config('rootpath');
+    var dest     = grunt.config('dest');
+    var src      = grunt.config('src');
 
     function Builder (id) {
         var self = this;
-        var defer = promise.Deferred();
 
         this.id = id;
         this.content = grunt.file.read(src + id);
-        this.ready = defer.done;
-        this.fail = defer.fail;
+        this.parserDefer = promise.Deferred();
 
         var parser = new Parser({
             compress: minify,
@@ -37,7 +37,8 @@ module.exports = function (grunt) {
 
         parser.parse(this.content, function (err, tree) {
             if (err) {
-                return defer.reject(err.message);
+                console.log(err.message);
+                return self.parserDefer.reject(err.message);
             }
 
             var imports = Object.keys(parser.imports.files)
@@ -53,22 +54,56 @@ module.exports = function (grunt) {
                 return children;
             };
 
-            self.build = function () {
-                var file = path.resolve(dest, id);
-
-                grunt.file.write(file, tree.toCSS({
-                    compress: minify
-                }));
-
-                return promise.Deferred().resolve([id]);
-            };
-
             self.isCmbFile = function () {
                 return !!children.length;
             };
-
-            defer.resolve();
+            self.parserDefer.resolve(tree);
         });
+    }
+
+    Builder.prototype.build = function () {
+        var self = this;
+        var defer = promise.Deferred();
+        var filename = self.id.replace(/\.less$/, '.css');
+
+        this.parserDefer.done(function (tree) {
+            var content = tree.toCSS({
+                compress: minify
+            });
+
+            content = rework(content)
+                .use(rework.url(function (url) {
+                    var match = url.match(HTTP_FILE_RE);
+                    var filepath;
+                    if (match && rootpaths.indexOf(match[1]) === -1) {
+                        return url;
+                    }
+
+                    // 获取文件的路径
+                    if (match) {
+                        filepath = url.replace(match[1], '');
+                    } else {
+                        filepath = path.normalize(path.dirname(self.id) + '/' + url);
+                    }
+                    filepath = URL.parse(filepath).pathname;
+
+                    var version = +require('fs').statSync(src + filepath).mtime % grunt.config('cacheExpire');
+
+                    return url + '?v=' + version;
+                }))
+                .toString();
+
+            try {
+                grunt.file.write(dest + filename, content);
+            } catch (ex) {
+                defer.resolve([]);
+            }
+
+            defer.resolve([filename]);
+        }).fail(function () {
+            defer.reject();
+        });
+        return defer.promise();
     }
 
     return Builder;
